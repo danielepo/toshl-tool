c77e8b3b65d20d85f191838d4ceaf4b76edbce20
danielepo <dnl.pozzobon@gmail.con>
2016-05-09 09:17:58 +0200

    Add Entry Loader

--- LoadToToshl/Program.fs
+++ LoadToToshl/Program.fs
@@ -2,8 +2,11 @@
 // See the 'F# Tutorial' project for more help.
 open Parser
 open System
+open ToshClient
 [<EntryPoint>]
 let main argv = 
-    loadMovimenti()
+    let tags = getTags()
+    let categories = getCategories();
+    let setEntry = setEntry 1.6 (DateTime(2016,05,09,9,0,0)) "caffe" 
     Console.ReadLine() |> ignore
     0 // return an integer exit code
--- ToshlClient/RestClient.fs
+++ ToshlClient/RestClient.fs
@@ -1,31 +1,146 @@
 ﻿module ToshClient
 open RestSharp
 open System
-let upload ()=
-    let client = new RestClient("http://example.com");
-    // client.Authenticator = new HttpBasicAuthenticator(username, password);
+type TagCount = {
+    entries:int;
+    unsorted_entries:int;
+    budgets:int
+}
 
-    let request = new RestRequest("resource/{id}", Method.POST)
-    request.AddParameter("name", "value") |> ignore // adds to POST or URL querystring based on Method
-    request.AddUrlSegment("id", "123") |> ignore // replaces matching token in request.Resource
+type Tag={
+  id:string;
+  name:string;
+  name_override:bool;
+  modified: DateTime;
+  ``type``: string;
+  category: string;
+  count: TagCount;
+  deleted: bool;
+  transient_created: DateTime;
+  transient_valid_till: string;
+  meta_tag: bool;
+  extra: obj
+}
+type CategoryCount = {
+    entries: int;
+    income_entries: int;
+    expense_entries: int;
+    tags_used_with_category: int;
+    income_tags_used_with_category: int;
+    expense_tags_used_with_category: int;
+    tags: int;
+    income_tags: int;
+    expense_tags: int;
+    budgets: int;
+}
+//type CategoryType =
+//    | expense
+//    | income
+//    | system
+type Category = {
+  id: string;
+  name:string;
+  modified: DateTime;
+  ``type``: string;
+  deleted: bool;
+  name_override:bool;
+  transient_created:DateTime;
+  transient_valid_till:string;
+  counts:CategoryCount;
+  extra: obj;
+}
+type Currency = {
+    code: string;
+}
 
-    // easily add HTTP Headers
-    request.AddHeader("header", "value") |> ignore
+type Location = {
+    id: string;
+    latitude: double;
+    longitude: double;
+}
+type Repeat = {
+    id: string;
+    start: DateTime;
+    ``end``: DateTime;
+    frequency: string;
+    interval: int;
+    count: int;
+    byday: string;
+    bymonthday: string;
+    bysetps: string;
+    iteration: double;
+}
+type Transaction = {
+    id: string;
+    account: string;
+    currency: Currency;
+}
+type Image = {
+    id: string;
+    path: string;
+    status: string;
+}
+type Reminder = {
+    period: string;
+    number: int;
+    at: string;
+}
+
+type Entry = {
+    amount: double;
+    currency: Currency;
+    date: DateTime;
+    desc: string;
+    account: string;
+    category: string;
+    tags: string list
+//    location: Location;
+//    repeat: Repeat;
+//    transaction: Transaction;
+//    images: Image list;
+//    reminders: Reminder list;
+    completed: bool;
+//    extra: obj;
+}
+    
+let addAuthorization (request:RestRequest) = 
+    request.AddHeader("Authorization", "Basic ZWYyZDAyMDktZjRiMC00NjAxLTk1NTQtOWY5MzI1NGFhNjlmNWFlYThhMjUtMWM4ZS00ZTcyLTk5NzUtNTFmMjQzNjlhNGYzOg==") |> ignore
+
+
+let client = new RestClient("https://api.toshl.com");
+let excecuteRequest (request:RestRequest) =
+    
+    addAuthorization request
 
     // execute the request
     let response = client.Execute(request)
-    let content = response.Content // raw content as string
+    response.Content // raw content as string
 
-    // or automatically deserialize result
-    // return content type is sniffed but can be explicitly set via RestClient.AddHandler();
-    let response2 = client.Execute(request)
-    let name = response2.Data.Name
+let getTags ()=
+    // client.Authenticator = new HttpBasicAuthenticator(username, password);
+    let request = new RestRequest("/tags", Method.GET)
+    let content = excecuteRequest request
+    Newtonsoft.Json.JsonConvert.DeserializeObject<Tag list>(content)
 
-    // easy async support
-    client.ExecuteAsync(request,(fun response -> printf "%s" (response.Content.ToString()))) |> ignore
+let getCategories()=
+    // client.Authenticator = new HttpBasicAuthenticator(username, password);
+    let request = new RestRequest("/categories", Method.GET)
+    let content = excecuteRequest request
+    Newtonsoft.Json.JsonConvert.DeserializeObject<Category list>(content)
+    
+let setEntry amount date description tag category =
+    let entry = {
+        amount = amount;
+        currency = {code = "EUR";};
+        date = date;
+        desc = description;
+        account = "";
+        category = category;
+        tags = [tag];
+        completed = true;
+    }
+    let request = new RestRequest("/entries",Method.POST)
+    let jsonEntry = Newtonsoft.Json.JsonConvert.SerializeObject(entry)
+    request.AddBody(jsonEntry) |> ignore
+    excecuteRequest request
 
-    // async with deserialization
-    let asyncHandle = client.ExecuteAsync(request,(fun response -> Console.WriteLine(response.Data.Name)))|> ignore
-
-    // abort the request on demand
-    asyncHandle.Abort();
