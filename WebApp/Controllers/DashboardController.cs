e3418d0cd4f83a934a8d5d7093a36c150fd9d746
Daniele Pozzobon <dnl.pozzobon@gmail.com>
2016-07-07 13:45:31 +0200

    Note di Progettazione della nuova funzionalità

--- WebApp/Controllers/DashboardController.cs
+++ WebApp/Controllers/DashboardController.cs
@@ -7,6 +7,13 @@ namespace WebApp.Controllers
     {
         public ActionResult Index()
         {
+            // scegli anno corrente
+            // carica tutte le spese fino a fine anno
+            // visualizzali in un grafico lineare
+            //  . colori differenti per tag differenti
+            //  . per i mesi a venire indica il valore medio dell'anno
+            //  . calcola i costi totali nell'anno per tag
+            //  .. per ogni tag poter indicare il tipo ricorrenza (o i mesi in cui ricorrere)
             return View();
         }
     }
