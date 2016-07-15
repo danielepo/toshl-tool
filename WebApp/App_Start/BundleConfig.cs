3e217f837d2df8bfe9b8579f48c90e8077f6e1e6
Daniele Pozzobon <dnl.pozzobon@gmail.com>
2016-07-15 13:36:51 +0200

    Migliorati i grafici con knockout

--- WebApp/App_Start/BundleConfig.cs
+++ WebApp/App_Start/BundleConfig.cs
@@ -11,7 +11,8 @@ namespace WebApp
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
             bundles.Add(new ScriptBundle("~/bundles/chartjs").Include(
-                        "~/Scripts/Chart.min.js"));
+                        "~/Scripts/Chart.min.js",
+                        "~/Scripts/knockout.chart.js"));
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
             bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
